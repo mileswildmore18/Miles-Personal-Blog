286ba2efda8cfa5b52f16176f72a9f716600c910
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-04 19:07:24 -0400

    Added a form to the index HTML and added a timer for when the warning message for the user to fill out the information before submitting. Also added the JavaScript array to validate the form if user submits their information.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -1,4 +1,34 @@
-////Add a Light/Dark mode
+//This will activate a alert message if submission form is not filled out
+const warningMessage = document.getElementsByClassName('form')
+
+//Show how long the alert message stays on failed submission
+function validateForm() {
+    let empty =document.getElementById(username).value;
+    if (empty==""){
+        alert(`${warningMessage}`)
+        return false;
+    }
+} 
+function countdown() {
+    let messageTime = 5;
+
+    const message = setInterval(function () {
+        
+        //updating the page
+        warningMessage.textContent = `${messageTime} please fill out the form`;
+        //updating the time of the message
+        
+        if (messageTime === 0) {
+            //Stop the timer
+            clearInterval(message);
+            //empty the box
+        warningMessage.textContent = '';
+        displayMessage();
+        }
+    })
+
+}
+//This will activate dark mode and light mode
 function darkMode() {
     let mode = document.body;
     mode.classList.toggle("dark-mode");
