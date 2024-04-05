974253acd2716db556aad20d979c990c61d78440
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-05 19:44:25 -0400

    Added the function for when the submit button is pressed and an error occurs via the JavaScript file for the Index. Also added the function for the submit button to communicate with the JavaScript file in the logic section.

--- Assets/js/logic.js
+++ Assets/js/logic.js
@@ -5,34 +5,57 @@ const userContent = document.querySelector('#content');
 const submitButton = document.querySelector('#submit');
 const warningMessage = document.getElementsByClassName('form');
 
+
 //Show how long the alert message stays on failed submission
 function validateForm() {
-    let empty =document.getElementById(username).value;
-    if (empty==""){
+    let empty = document.getElementById(username).value;
+    if (empty == "") {
         alert(`${warningMessage}`)
         return false;
     }
-} 
+}
 
-submitButton.addEventListener('click', function (event){
-    let userName = localStorage.getItem('')
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    //Getting values from the form
+    const userName = document.querySelector('#username').value;
+    const title = document.querySelector('#title').value;
+    const content = document.querySelector('#content').value;
+
+    if (userName === '') {
+        displayMessage('Please fill out the form before submitting');
+    } else if (title === '') {
+        displayMessage('Please fill out the form before submitting');
+    } else if (content === '') {
+        displayMessage('Please fill out the form before submitting');
+    } else {
+        function redirect() {
+            window.location.replace("./blog.html");
+            return false;
+
+            localStorage.setItem('username', userName);
+            localStorage.setItem('title', title);
+            localStorage.setItem('content', content)
+        }
+    }
 })
+
 
 function countdown() {
     let messageTime = 5;
 
     const message = setInterval(function () {
-        
+
         //updating the page
         warningMessage.textContent = `Please fill out the form before submitting`;
         //updating the time of the message
-        
+
         if (messageTime === 0) {
             //Stop the timer
             clearInterval(message);
             //empty the box
-        warningMessage.textContent = '';
-        displayMessage();
+            warningMessage.textContent = '';
+            displayMessage();
         }
     })
 
